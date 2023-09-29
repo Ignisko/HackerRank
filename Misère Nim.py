82eaffb807b25aae2b5d32e8e65a9d0aaa4c464b
Ignisko <58820214+Ignisko@users.noreply.github.com>
2023-09-29 19:56:27 +0200

    Add files via upload
    # add Misère Nim HR solution

---
+++ Misère Nim.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+def misereNim(s):
+    
+    if (set(s))=={1} and n %2 ==1:
+        return "Second"
+    elif (set(s))=={1} and n %2 == 0:
+        return "First"
+    elif reduce((lambda x, y: x ^ y), s):
+
+        return "First"
+    else:
+        return "Second"
+        
+        

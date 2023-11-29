2ae245a6c07ba682cc2d37bf9b5def481370c0b0
Ignisko <58820214+Ignisko@users.noreply.github.com>
2023-11-29 21:21:10 +0100

    Create Array Manipulation.py
    #add solution for the "Array Manipulation" problem
    #python3
    #"prefix sum" aka "difference array" technique
    #
    #mark the start and end of the range with the value to add
    #accumulate these values through the array to get the final array state

---
+++ Array Manipulation.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    
+    arr = [0]*n
+    for a, b, k in queries:
+        arr[a-1] += k
+        if b < n:
+            arr[b] -= k
+    max_value, current_sum = 0, 0
+    for value in arr:
+        current_sum += value
+        max_value = max(max_value, current_sum)
+    return max_value
+    
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
